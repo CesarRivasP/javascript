2562b431b83c0c5d836e3d2dcdc9b4d8658ea72e
CesarRivasP <cesarjose58@gmail.com>
2019-05-14 12:08:19 -0400

    Delete comments and added template strings practice

--- udemy/01-variables/js/app.js
+++ udemy/01-variables/js/app.js
@@ -1,26 +1,26 @@
 // variables con var
-//var aprendiendo = 'Javascript';
+var aprendiendo = 'Javascript';
 
 // variables con const
 
-//const aprendiendo; // NO --- Las constantes siempre deben tener un valor de  inicio
+const aprendiendo; // NO --- Las constantes siempre deben tener un valor de  inicio
 // Al ser una constante sus valores nunca cambian
 
 // variables con let
-//let aprendiendo; // Si -- se puede inicializar sin un valor
+let aprendiendo; // Si -- se puede inicializar sin un valor
 //tambien se puede reescribir su valor
 
 // --------------------------------------------------------------- //
 
 // Scope
 
-// var musica = 'rock'
-//
-// if(musica){
-//   var musica = 'rap'; //reescribe totalmente la variable
-//   console.log(`dentro del if : ${musica}`);
-// }
-// console.log(`fuera del if : ${musica}`);
+var musica = 'rock'
+
+if(musica){
+  var musica = 'rap'; //reescribe totalmente la variable
+  console.log(`dentro del if : ${musica}`);
+}
+console.log(`fuera del if : ${musica}`);
 
 // ------------------------------------------------------------------ //
 
@@ -33,3 +33,26 @@ if(musica){
   console.log(`dentro del if : ${musica}`);
 }
 console.log(`fuera del if : ${musica}`);
+
+// ------------------------------------------------------------------ //
+
+//  ***** Sin Template String  *****
+const name = 'César';
+const job = 'Programming';
+                  //seleccion de elementos en javascript
+const containerApp = document.querySelector('#app');
+
+let html = '<ul>' +
+              '<li>Nombre: ' + name + '</li>' +
+              '<li>Trabajo: ' + job + '</li>' +
+            '</ul>';
+
+containerApp.innerHTML = html;
+
+// *****  Con Template String  *****
+let html = `<ul>
+              <li>Nombre: ${name}</li>
+              <li>Trabajo: ${job}</li>
+            </ul>`;
+
+containerApp.innerHTML = html;
