0548895c760d8eeb86da23a47a88e33ad9780073
CesarRivasP <cesarjose58@gmail.com>
2019-05-14 15:08:32 -0400

    Added class about Object Constructor

--- udemy/01-variables/js/app.js
+++ udemy/01-variables/js/app.js
@@ -8,6 +8,7 @@ console.log(namePerson);
 console.log(career);
 
 // Usando objetos
+//  * Object Literal -
 const person = {
   name: 'César',
   career: 'React Developer'
@@ -19,5 +20,18 @@ console.log(person['name']);  //otra forma de acceder a un valor dentro de un ob
 
 // ------------------------------------------------------------------------------------ //
 
-// Object Constructor
-// Permite crear objetos con distintos valores, es la manera antigua de realizar esta tarea
+// - Object Constructor -
+// Permite crear multipes objetos con distintos valores
+// *** Es la manera antigua de realizar esta tarea **
+function Todo(name, urgency){
+  this.name = name;
+  this.urgency = urgency;
+}
+
+const Todo1 = new Todo('Aprender JS y React', 'Urgente');
+const Todo2 = new Todo('Comprar medicinas', 'Media');
+const Todo3 = new Todo('Desplegar app react native', 'baja');
+console.log(Todo1);
+console.log(Todo2);
+console.log(Todo3);
+console.log(Todo1.name);
