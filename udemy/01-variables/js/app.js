722c733b1eadb5bdad2c8b482f230cdbaab496e3
CesarRivasP <cesarjose58@gmail.com>
2019-05-14 15:02:05 -0400

    Added objets class

---
+++ udemy/01-variables/js/app.js
@@ -0,0 +1,23 @@
+// Objetos
+
+// Antes de usar objetos
+let namePerson = 'César';
+let career = 'React Developer';
+
+console.log(namePerson);
+console.log(career);
+
+// Usando objetos
+const person = {
+  name: 'César',
+  career: 'React Developer'
+}
+
+console.log(person);
+console.log(person.name);
+console.log(person['name']);  //otra forma de acceder a un valor dentro de un objeto
+
+// ------------------------------------------------------------------------------------ //
+
+// Object Constructor
+// Permite crear objetos con distintos valores, es la manera antigua de realizar esta tarea
