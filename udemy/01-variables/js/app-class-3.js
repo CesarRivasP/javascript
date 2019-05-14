62a83db8a76477c0f4d97aa36f130b6112123ce7
CesarRivasP <cesarjose58@gmail.com>
2019-05-14 18:13:20 -0400

    Rename file and added prototypes class

--- udemy/01-variables/js/app-class-3.js
+++ udemy/01-variables/js/app-class-3.js
@@ -35,3 +35,31 @@ console.log(Todo1);
 console.log(Todo2);
 console.log(Todo3);
 console.log(Todo1.name);
+
+
+function displayTodoInfo(todo, priority){
+  return `La tarea ${todo} tiene una prioridad de ${priority}`
+}
+
+const displayInfo = displayTodoInfo(Todo1.name, Todo1.urgency)
+console.log(displayInfo);
+
+// ------------------------------------------------------------------------------------ //
+
+// Prototypes
+function Todo(name, urgency){
+  this.name = name;
+  this.urgency = urgency;
+}
+
+Todo.prototype.displayTodoInfo = function(){  //como ya es parte del objeto, no es necesario pasarle parametros
+  // para acceder a ellos
+  // displayTodoInfo esta completamente apartado para trabajar con objetos de tipo Todo
+  return `La tarea ${this.name} tiene una prioridad de ${this.urgency}`;
+}
+
+const Todo1 = new Todo('Aprender JS y React', 'Urgente');
+console.log(Todo1);
+console.log(Todo1.displayTodoInfo());
+const Todo2 = new Todo('Comprar medicinas', 'Media');
+console.log(Todo2.displayTodoInfo());
