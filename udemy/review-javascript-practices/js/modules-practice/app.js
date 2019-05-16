40c07b542bfa8d6031043b2201f3524217b86b68
CesarRivasP <cesarjose58@gmail.com>
2019-05-16 10:40:13 -0400

    Added export class notes

--- udemy/review-javascript-practices/js/modules-practice/app.js
+++ udemy/review-javascript-practices/js/modules-practice/app.js
@@ -1,9 +1,18 @@
-import { todoName, createTodo, completeTodo } from './todos.js';
-// import todoName from './todos.js';
+// import { todoName, createTodo, completeTodo } from './todos.js';
+// // import todoName from './todos.js';
+//
+// console.log(todoName);
+//
+// const todo1 = createTodo('Pasear al perro', "Media")
+// console.log(todo1);
+//
+// completeTodo();
 
-console.log(todoName);
 
-const todo1 = createTodo('Pasear al perro', "Media")
+import Todo from './todos.js';
+
+const todo1 = new Todo('Learning JS');
+
 console.log(todo1);
 
-completeTodo();
+todo1.display()
