9d3a0c84e358445a829f9886284b36eba682d59b
CesarRivasP <cesarjose58@gmail.com>
2019-05-16 10:40:24 -0400

    Create buys class

---
+++ udemy/review-javascript-practices/js/modules-practice/buys.js
@@ -0,0 +1,13 @@
+// HERENCIA
+class Buys extends Todo {
+  constructor(name, priority, cant){
+    super(name, priority);  //Al super constructor se le pasan los atributos de la clase que se esta heredando
+    this.cant = cant;
+  }
+  // Tambien se heredan los metodos
+  // Tambien se pueden alterar
+  display(){
+    super.display();  //reescribir un metodo
+    console.log( `y la cantidad de ${this.cant}`);
+  }
+}
