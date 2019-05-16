8d9c1bf9c8e125c1984f224a8f62b2f741fa71f4
CesarRivasP <cesarjose58@gmail.com>
2019-05-16 10:29:40 -0400

    Create app.js

---
+++ udemy/review-javascript-practices/js/modules-practice/app.js
@@ -0,0 +1,9 @@
+import { todoName, createTodo, completeTodo } from './todos.js';
+// import todoName from './todos.js';
+
+console.log(todoName);
+
+const todo1 = createTodo('Pasear al perro', "Media")
+console.log(todo1);
+
+completeTodo();
